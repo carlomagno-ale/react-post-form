634e547c7bd62b344d5b1055cf3492d4ce63f165
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-31 16:48:32 +0200

    Implement controlled form components for post submission

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,32 @@ import { useState } from 'react'
 
 export default function App() {
 
+  const [formData, setFormData] = useState({
+    author: '',
+    title: '',
+    body: '',
+    public: ''
+  })
 
+
+  function handleFormData(e) {
+    const type = e.target.type;
+    const value = type === 'checkbox' ? e.target.checked : e.target.value;
+    const key = e.target.id;
+
+
+    console.log(type, key, value)
+    console.dir(e.target);
+
+    setFormData({ ...formData, [key]: value })
+  }
+
+  function handleFormSubmit(e) {
+
+    e.preventDefault()
+    console.log(formData)
+
+  }
 
   return (
     <>
@@ -13,46 +38,59 @@ export default function App() {
       </header>
 
       <main>
-        <form className="container">
-          <div className="col-12 pt-3">
-            <input
-              type="text"
-              className="form-control"
-              id="author"
-              placeholder="Autore del post" />
-          </div>
+        <div className='container'>
+          <form method='POST' onSubmit={handleFormSubmit}>
+            <div className="col-12 pt-3">
+              <input
+                type="text"
+                className="form-control"
+                id="author"
+                placeholder="Autore del post"
+                value={formData.author}
+                onChange={handleFormData}
+                required />
+            </div>
 
-          <div className="col-12 my-3">
-            <input
-              type="text"
-              className="form-control"
-              id="title"
-              placeholder="Titolo del post" />
-          </div>
+            <div className="col-12 my-3">
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                placeholder="Titolo del post"
+                value={formData.title}
+                onChange={handleFormData}
+                required />
+            </div>
 
-          <div className="col-12">
-            <textarea
-              className="form-control"
-              id="body"
-              rows="3"
-              placeholder="Testo del post">
-            </textarea>
-          </div>
+            <div className="col-12">
+              <textarea
+                className="form-control"
+                id="body"
+                rows="3"
+                placeholder="Testo del post"
+                value={formData.body}
+                onChange={handleFormData}
+                required >
+              </textarea>
+            </div>
 
-          <div className="mb-3 form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="public" />
-            <label
-              className="form-check-label"
-              htmlFor="public"> Public
-            </label>
-          </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="public"
+                value={formData.public}
+                onChange={handleFormData} />
+              <label
+                className="form-check-label"
+                htmlFor="public"> Public
+              </label>
+            </div>
 
-          <button type="submit" className="btn btn-primary">Invia</button>
+            <button type="submit" className="btn btn-primary">Invia</button>
 
-        </form>
+          </form>
+        </div>
       </main>
     </>
   )
