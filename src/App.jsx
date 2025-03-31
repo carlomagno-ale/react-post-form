61875ed6e973171054e3bee7484ad3f8c786ef74
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-31 15:58:20 +0200

    Add post form layout and styling to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,58 @@ import { useState } from 'react'
 
 export default function App() {
 
+
+
   return (
     <>
+      <header>
+        <div className='container'>
+          <h1 className='text-center py-2'>React Post Form</h1>
+        </div>
+      </header>
 
+      <main>
+        <form className="container">
+          <div className="col-12 pt-3">
+            <input
+              type="text"
+              className="form-control"
+              id="author"
+              placeholder="Autore del post" />
+          </div>
+
+          <div className="col-12 my-3">
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              placeholder="Titolo del post" />
+          </div>
+
+          <div className="col-12">
+            <textarea
+              className="form-control"
+              id="body"
+              rows="3"
+              placeholder="Testo del post">
+            </textarea>
+          </div>
+
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="public" />
+            <label
+              className="form-check-label"
+              htmlFor="public"> Public
+            </label>
+          </div>
+
+          <button type="submit" className="btn btn-primary">Invia</button>
+
+        </form>
+      </main>
     </>
   )
 }
